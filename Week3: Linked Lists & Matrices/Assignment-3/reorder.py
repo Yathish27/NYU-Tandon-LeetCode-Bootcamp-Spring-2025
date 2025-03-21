bd5eb3f28a73d62a01ea7c45a38929c4e4ab9dde
Yathish N V <yn2426@nyu.edu>
2025-03-20 23:01:57 -0400

    Create reorder.py

---
+++ Week3: Linked Lists & Matrices/Assignment-3/reorder.py
@@ -0,0 +1,37 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if not head or not head.next:
+            return
+        
+        # Step 1: Find the middle of the linked list
+        slow, fast = head, head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        # Step 2: Reverse the second half of the linked list
+        prev, curr = None, slow.next
+        while curr:
+            tmp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = tmp
+        
+        slow.next = None  # Cut the list into two halves
+        
+        # Step 3: Merge the two halves alternately
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
+        
